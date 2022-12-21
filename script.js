d4dbdc892a0a18b0c30378fa422ca045c6448462
sebastianocalella <sebastianocalella97@gmail.com>
2022-12-21 15:31:36 +0100

    assigned random email to Vue data element

--- script.js
+++ script.js
@@ -3,14 +3,18 @@ const { createApp } = Vue
 createApp({
     data() {
     return {
-        emailList : []
+        emailList : [],
+        actualEmail : 1
     }
     },
     methods:{
         getRandomEmail(){
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
             .then( (response) => {
+                console.log(this.actualEmail)
                 console.log(response)
+                this.actualEmail = response.data.response
+                console.log(this.actualEmail)
             })
         },
     },
